eddeb1a7f92f9f833fdba604950b999b3d8b462b
atimecalledu <umaimaali126@gmail.com>
2025-02-16 01:35:36 +0500

    first commit

---
+++ auth_app/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render ,HttpResponse,redirect
+from django.contrib.auth.models import User 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+@login_required(login_url='login')
+def homepage(request):
+
+     return render(request,'home.html')
+
+def signup_page(request):
+      if request.method=='POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+    
+
+        if pass1!=pass2:
+            return HttpResponse("password does not match")
+        
+        else:
+            my_user =User.objects.create_user(uname,email,pass1)
+        my_user.save()
+        return redirect('login')
+    
+      
+      return render(request,'signup.html')
+
+def login_page(request):
+
+    if request.method=='POST': #as written in form of login
+        username1=request.POST.get('username')
+        pass1=request.POST.get('pass')
+        user=authenticate(request,username= username1 ,password=pass1)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return HttpResponse("username and password is incorrect")
+    return render(request,'login.html')
+
+def logout_page(request):
+
+    logout(request)
+    return redirect('login')
+ 
